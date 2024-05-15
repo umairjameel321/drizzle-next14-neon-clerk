15c281424c73924c2ef8af6ba707f93662e7ec02
Umair Jameel <umairjameel321@gmail.com>
2024-05-15 22:25:09 +0500

    fix error.

--- actions/userActions.ts
+++ actions/userActions.ts
@@ -10,9 +10,9 @@ export const getAllUsers = async () => {
   return data;
 };
 
-export const getUser = async (userId: number) => {
+export const getUser = async (userId: any) => {
   const user = await db.query.users.findMany({
-    where: (users, { eq }) => eq(users.id, userId),
+    where: (users, { eq }) => eq(users.clerkId, userId),
     with: {
       todos: true,
     },
