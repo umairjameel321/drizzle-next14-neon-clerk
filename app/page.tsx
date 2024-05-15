6e88d3c446e70c55c0d409150327047dcddb9fea
Umair Jameel <umairjameel321@gmail.com>
2024-05-15 22:25:09 +0500

    fix error.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import { getData } from "@/actions/todoActions";
 import { getAllUsers, getUser } from "@/actions/userActions";
 import Todos from "@/components/Todos";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 export default async function Home() {
-  // const users = await getAllUsers();
-  // console.log(users);
-  // const data = await getData(users[0].id);
-  // const user = await getUser(users[0].id);
-
-  // console.log(user);
+  const user: any = await currentUser();
+  if (!user) return;
+  const fetchedData = await getUser(user?.id);
+  console.log(fetchedData);
 
   return (
-    <main className="flex  items-center justify-between ">
-      {/* <Todos todos={data} user={users[0]} /> */}
-    </main>
+    fetchedData && (
+      <main className="flex  items-center justify-between ">
+        <Todos todos={fetchedData[0].todos} user={fetchedData[0]} />
+      </main>
+    )
   );
 }
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -23,7 +23,7 @@ const Todos: FC<Props> = ({ todos, user }) => {
   // Function to create a new todo item
   const createTodo = (text: string) => {
     // addUser();
-    const id = (todoItems.at(-1)?.id || 0) + 1;
+    const id = new Date().getTime();
     addTodo(id, text, user?.id);
     setTodoItems((prev) => [
       ...prev,
